7c3c079c824d51b10cd7a550803c77c7f7b2e891
bw4sz <benweinstein2010@gmail.com>
2016-02-03 16:17:02 -0800

    writing simdat

--- NegativeBinomial.R
+++ NegativeBinomial.R
@@ -51,7 +51,8 @@ days<-3
 #set dispersion parameter, get from command line
 args <- commandArgs(TRUE)
 print(args)
-dispersion<-as.numeric(args[1])
+#dispersion<-as.numeric(args[1])
+dispersion=1
 
 #Bill sizes
 Bill<-rpois(h_species,10)
@@ -153,139 +154,10 @@ ggplot(obs.state,aes(x=Interactions,y=Yobs,col=Camera)) + geom_point() + theme_b
 #   $$\sigma_{intercept} = \sqrt[2]{\frac{1}{\tau_\alpha}}$$
 #   $$\sigma_{slope} = \sqrt[2]{\frac{1}{\tau_\beta}}$$
   
-  #Simulated data without detection
-  
-#runs<-70000
-runs<-10000
-
-#trigger parallel
-paralleljags<-T
-
-#Source model
-source("Bayesian/NoDetectNmixturePoissonRagged.R")
-
-#print model
-print.noquote(readLines("Bayesian//NoDetectNmixturePoissonRagged.R"))
-
-if(paralleljags){
-  
-  #for parallel run
-  Yobs=obs.state$Yobs
-  Bird=obs.state$Bird
-  Plant=obs.state$Plant
-  Plants=max(obs.state$Plant)
-  Camera=obs.state$Camera
-  Day=obs.state$Day
-  Traitmatch<-traitmatch
-  #number of birds to iterate
-  Birds=max(obs.state$Bird)
-  Nobs=length(obs.state$Yobs)
-  
-  #A blank Y matrix - all present
-  Ninit<-rep(max(obs.state$Yobs)+1,Nobs)
-  
-  #Inits
-  InitStage <- function() {list(beta=rep(0.5,Birds),alpha=rep(0.5,Birds),intercept=0,tau_alpha=0.1,tau_beta=0.1,N=Ninit,gamma=0)}
-  
-  #Parameters to track
-  ParsStage <- c("alpha","beta","intercept","sigma_int","sigma_slope","ynew","gamma","fit","fitnew")
-  
-  #MCMC options
-  ni <- runs  # number of draws from the posterior
-  nt <- 5   #thinning rate
-  nb <- runs*.95 # number to discard for burn-in
-  nc <- 2  # number of chains
-  
-  Dat<-list("Yobs","Bird","Plant","Plants","Traitmatch","Birds","Nobs","Ninit")
-  
-  sim_niave<-do.call(jags.parallel,list(Dat,InitStage,ParsStage,model.file="Bayesian/NoDetectNmixturePoissonRagged.jags",n.thin=nt, n.iter=ni,n.burnin=nb,n.chains=nc))
-  
-} else {
-  #Input Data
-  Dat <- list(
-    Yobs=obs.state$Yobs,
-    Bird=obs.state$Bird,
-    Plant=obs.state$Plant,
-    Plants=max(obs.state$Plant),
-    Time=obs.state$Time,
-    Traitmatch=traitmatch,
-    #number of birds to iterate
-    Birds=max(obs.state$Bird),
-    Nobs=length(obs.state$Yobs))
-  
-  #A blank Y matrix - all present
-  Ninit<-rep(max(Dat$Yobs)+1,Dat$Nobs)
-  
-  #Inits
-  InitStage <- function() {list(beta=rep(0.5,Dat$Birds),alpha=rep(0.5,Dat$Birds),dtrans=rep(0,Dat$Birds),intercept=0,tau_alpha=0.1,tau_beta=0.1,N=Ninit,gamma=0)}
-  
-  #Parameters to track
-  ParsStage <- c("detect","alpha","beta","intercept","sigma_int","sigma_slope","ynew","gamma","fit","fitnew")
-  
-  #MCMC options
-  ni <- runs  # number of draws from the posterior
-  nt <- 5   #thinning rate
-  nb <- runs*.95 # number to discard for burn-in
-  nc <- 2  # number of chains
-  
-  #Jags
-  
-  sim_niave <- jags(inits=InitStage,
-                    n.chains=nc, model.file="Bayesian/NoDetectNmixturePoissonRagged.jags",
-                    working.directory=getwd(),
-                    data=Dat,
-                    parameters.to.save=ParsStage,
-                    n.thin=nt,
-                    n.iter=ni,
-                    n.burnin=nb,
-                    DIC=T)
-}
-
-pars_niave<-extract_par(sim_niave,data=obs.state)
-pars_niave$Model<-c("Poisson GLMM")
-
-##Assess Convergence
-
-ggplot(pars_niave[pars_niave$par %in% c("detect","alpha","beta"),],aes(x=Draw,y=estimate,col=as.factor(Chain))) + geom_line() + facet_grid(par~species,scale="free") + theme_bw() + labs(col="Chain") + ggtitle("Detection Probability")
-
-ggplot(pars_niave[pars_niave$par %in% c("gamma","sigma_int","sigma_slope"),],aes(x=Draw,y=estimate,col=as.factor(Chain))) + geom_line() + theme_bw() + labs(col="Chain") + ggtitle("Trait-matching regression") + facet_wrap(~par,scales="free")
-
-##Posteriors
-
-###Posterior Distributions
-p<-ggplot(pars_niave[pars_niave$par %in% c("alpha","beta"),],aes(x=estimate)) + geom_histogram() + ggtitle("Estimate of parameters") + facet_grid(species~par,scales="free") + theme_bw() + ggtitle("Species Posteriors")
-
-#Add true values
-tr<-melt(data.frame(species=1:length(detection),alpha=alpha,beta=beta),id.var='species')
-colnames(tr)<-c("species","par","value")
-psim<-p + geom_vline(data=tr,aes(xintercept=value),col='red',linetype='dashed',size=1)
-psim
-
-p<-ggplot(pars_niave[pars_niave$par %in% c("gamma","intercept","sigma_int","sigma_slope"),],aes(x=estimate)) + geom_histogram() + ggtitle("Hierarchical Posteriors") + facet_grid(~par,scale="free") + theme_bw()
-
-#Add true values
-tr<-melt(list(gamma=gamma,intercept=intercept,sigma_int=sigma_intercept,sigma_slope=sigma_slope))
-
-colnames(tr)<-c("value","par")
-
-psim2<-p + geom_vline(data=tr,aes(xintercept=value),linetype='dashed',size=1,col="red")
-
-#**True values are given in the dashed lines.**
-  
-  ##Predicted Relationship 
-  
-castdf<-group_by(pars_niave,Chain) %>% dplyr::select(par,estimate) %>% filter(par %in% c("gamma","intercept"))
-
-castdf<-dcast(pars_niave[pars_niave$par %in% c("gamma","intercept"),], Chain + Draw~par,value.var="estimate")
-
-#calculated predicted y
-predyniave<-trajF(alpha=castdf$intercept,beta=castdf$gamma,x=as.numeric(traitmatch))
-
-orig<-trajF(alpha=rnorm(2000,intercept,sigma_intercept),beta=rnorm(2000,gamma,sigma_slope),x=as.numeric(traitmatch))
 
 # Simulated data with detection
 
-runs<-10000
+runs<-1000
 
 #trigger parallel
 paralleljags<-T
@@ -377,6 +249,44 @@ if(paralleljags){
 pars<-extract_par(sim_detect,data=obs.state)
 pars$Model<-"Occupancy"
 
-pO<-rbind_all(list(pars_niave,pars))
 filename<-paste("Dispersion/Allpars",dispersion,".csv",sep="")
-write.csv(pO,filename)
+
+write.csv(pars,filename)
+
+#true number of observed interactions
+true_state<-obs.state %>% group_by(Bird,Plant) %>% summarize(n=sum(Yobs)) %>% acast(.$Bird~.$Plant)
+
+Ndetect<-pars[pars$par %in% "ynew",]
+bydraw<-split(Ndetect,list(Ndetect$Chain,Ndetect$Draw))
+occ_matrix<-lapply(bydraw,function(x){
+  r<-acast(x,species ~ plant,value.var = "estimate",fun.aggregate = sum)
+})
+
+#calculate discrep for those aggregated matrices
+occ<-lapply(occ_matrix,function(r){
+  #for each position what is the chisq
+  rmerge<-matrix(nrow = nrow(true_state),ncol=ncol(true_state))
+  for (x in 1:nrow(r)){
+    for (y in 1:ncol(r)){
+      rmerge[x,y]<-chisq(e=r[x,y],o=true_state[x,y])
+    }
+  }
+  return(rmerge)
+})
+
+names(occ)<-1:length(occ)
+names(occ_matrix)<-1:length(occ_matrix)
+
+##Deviates
+mmat<-melt(true_state)
+colnames(mmat)<-c("Bird","Plant","True_State")
+
+#append to predicted matrices
+
+#occupancy with detection
+mocc<-melt(occ_matrix)
+colnames(mocc)<-c("Bird","Plant","Occupancy","Iteration")
+simdat<-merge(mocc,mmat,by=c("Bird","Plant"),all.x=T)
+
+filename<-paste("Dispersion/simdat",dispersion,".csv",sep="")
+
