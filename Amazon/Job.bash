2c40f671a7ea9807bb736549eac6fce663004577
bw4sz <benweinstein2010@gmail.com>
2016-07-28 12:57:02 -0700

    end of line

--- Amazon/Job.bash
+++ Amazon/Job.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
 #cd into occupy, if directory doesn't exist, kill the run.
-git clone git@github.com:bw4sz/Occupy.git --depth 1
-
 cd Occupy||sudo halt
 
 #git pull to make sure we are at HEAD
